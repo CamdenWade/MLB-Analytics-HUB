f18b5bc74279fe1db51c0fedf7d04f5c6cf84e6a
CamdenWade <ckw5610@psu.edu>
2026-05-06 12:37:32 -0400

    Fixing Packages File

--- R/00_packages.R
+++ R/00_packages.R
@@ -1,34 +1,22 @@
-packages <- c(
+options(repos = c(CRAN = "https://cloud.r-project.org"))
+
+required_packages <- c(
   "shiny",
   "bslib",
   "tidyverse",
-  "baseballr",
+  "dplyr",
+  "ggplot2",
   "DT",
-  "plotly",
-  "scales",
-  "janitor",
-  "quarto",
   "gt",
-  "broom"
+  "broom",
+  "forcats",
+  "scales",
+  "stringr",
+  "purrr"
 )
 
-library(shiny)
-library(bslib)
-library(tidyverse)
-library(DT)
-library(plotly)
-library(ggplot2)
-library(gt)
-library(quarto)
-library(broom)
-library(forcats)
-library(scales)
-
-install_if_missing <- function(pkg) {
-  if (!requireNamespace(pkg, quietly = TRUE)) {
-    install.packages(pkg)
-  }
-}
-
-invisible(lapply(packages, install_if_missing))
-invisible(lapply(packages, library, character.only = TRUE))
+invisible(lapply(required_packages, function(pkg) {
+  suppressPackageStartupMessages(
+    library(pkg, character.only = TRUE)
+  )
+}))
